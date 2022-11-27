5788a732c42ff429ac68af183b853558ada072a0
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-11-27 14:32:28 -0700

    commit tree 9a82ab45c94399c5c8d4d116b46f0a6649aa786e

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,16 @@
 class PostsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordInvalid, with: :display_error
+
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -18,6 +19,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def display_error(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
